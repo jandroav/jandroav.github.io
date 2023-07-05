07723655dc0632aeca45ecbec41272eefa94d6a6
jandroav <jandroav@icloud.com>
2023-07-05 13:09:06 +0200

    style(styles.css): add new CSS rules for card__cert to style the certifications card script(main.js): add new JavaScript functions to handle the show/hide functionality for the certifications card index.html: add HTML markup for the certifications card, commented out for now

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,3 +19,25 @@ const showSocial = (toggleCard, socialCard) =>{
    })
 }
 showSocial('card-toggle','card-social')
+
+/*=============== SHOW CERTS  ===============*/
+const showCerts = (toggleCard, certsCard) =>{
+    const toggle = document.getElementById(toggleCard),
+          social = document.getElementById(certsCard)
+    
+    toggle.addEventListener('click', ()=>{
+        // If the animation class exists, we add the down-animation class
+        if(social.classList.contains('animation')){
+            social.classList.add('down-animation')
+ 
+            // We remove the down-animation class
+            setTimeout(() =>{
+                social.classList.remove('down-animation')
+            }, 1000)
+        }
+ 
+        // We add the animation class to the div tag with the card__social class
+        social.classList.toggle('animation')
+    })
+ }
+ showCerts('card-cert-toggle','card-cert')
