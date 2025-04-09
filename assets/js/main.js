a490d373725395c49a6ca0fef2294b1d469be583
Alejandro Alvarez <github@jandroav.net>
2025-04-09 07:40:33 +0200

    ✨ (styles.css, main.js, index.html): rename social-related classes to connect for better clarity and update styles accordingly 📝 (styles.css): fix CSS variable for smaller font size to use leading zero for consistency

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,43 +1,43 @@
-/*=============== SHOW SOCIAL NETWORKS ===============*/
-const showSocial = (toggleCard, socialCard) =>{
-   const toggle = document.getElementById(toggleCard),
-         social = document.getElementById(socialCard)
-   
-   toggle.addEventListener('click', ()=>{
-       // If the animation class exists, we add the down-animation class
-       if(social.classList.contains('animation')){
-           social.classList.add('down-animation')
+/*=============== SHOW NETWORKS ===============*/
+const showConnect = (toggleCard, connectCard) => {
+  const toggle = document.getElementById(toggleCard),
+    connect = document.getElementById(connectCard);
 
-           // We remove the down-animation class
-           setTimeout(() =>{
-               social.classList.remove('down-animation')
-           }, 1000)
-       }
+  toggle.addEventListener("click", () => {
+    // If the animation class exists, we add the down-animation class
+    if (connect.classList.contains("animation")) {
+      connect.classList.add("down-animation");
 
-       // We add the animation class to the div tag with the card__social class
-       social.classList.toggle('animation')
-   })
-}
-showSocial('card-toggle','card-social')
+      // We remove the down-animation class
+      setTimeout(() => {
+        connect.classList.remove("down-animation");
+      }, 1000);
+    }
+
+    // We add the animation class to the div tag with the card__connect class
+    connect.classList.toggle("animation");
+  });
+};
+showConnect("card-toggle", "card-connect");
 
 /*=============== SHOW CERTS  ===============*/
-const showCerts = (toggleCard, certsCard) =>{
-    const toggle = document.getElementById(toggleCard),
-          social = document.getElementById(certsCard)
-    
-    toggle.addEventListener('click', ()=>{
-        // If the animation class exists, we add the down-animation class
-        if(social.classList.contains('animation')){
-            social.classList.add('down-animation')
- 
-            // We remove the down-animation class
-            setTimeout(() =>{
-                social.classList.remove('down-animation')
-            }, 1000)
-        }
- 
-        // We add the animation class to the div tag with the card__social class
-        social.classList.toggle('animation')
-    })
- }
- showCerts('card-cert-toggle','card-cert')
+const showCerts = (toggleCard, certsCard) => {
+  const toggle = document.getElementById(toggleCard),
+    social = document.getElementById(certsCard);
+
+  toggle.addEventListener("click", () => {
+    // If the animation class exists, we add the down-animation class
+    if (social.classList.contains("animation")) {
+      social.classList.add("down-animation");
+
+      // We remove the down-animation class
+      setTimeout(() => {
+        social.classList.remove("down-animation");
+      }, 1000);
+    }
+
+    // We add the animation class to the div tag with the card__cert class
+    social.classList.toggle("animation");
+  });
+};
+showCerts("card-cert-toggle", "card-cert");
